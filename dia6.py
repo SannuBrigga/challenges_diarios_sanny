9deaf70f825001aadeae3105c7a25027c82fa308
SannuBrigga <149159099+SannuBrigga@users.noreply.github.com>
2024-06-18 20:30:56 -0400

    Add files via upload

---
+++ dia6.py
@@ -0,0 +1,3 @@
+celcius= float(input("ingrese una temperatura por favor: ")) #pido que se ingrese una temperatira en celcius
+fare= (celcius*(9/5))+32 #creo la variable  fahrenheit y pongo la formula 
+print (fare) #imprimo el resultado
