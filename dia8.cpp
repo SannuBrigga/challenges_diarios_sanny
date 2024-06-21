6946b6ab17b6b63e0237ef4db2d87a7c56b5a962
SannuBrigga <149159099+SannuBrigga@users.noreply.github.com>
2024-06-21 18:55:52 -0400

    Add files via upload

---
+++ dia8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, suma;
+
+    // Leer el primer número desde la entrada del usuario
+    cout << "Ingrese el primer numero: ";
+    cin >> n1;
+
+    // Leer el segundo número desde la entrada del usuario
+    cout << "Digite el segundo numero: ";
+    cin >> n2;
+
+    // Calcular la suma de los dos números
+    suma = n1 + n2;
+
+    // Mostrar el resultado de la suma
+    cout << "La suma es " << suma << endl;
+
+    // Indicar que el programa terminó correctamente
+    return 0;
+}
